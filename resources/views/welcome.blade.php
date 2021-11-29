f3865e7e8650142b364be04318318fefc32b9b85
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-29 12:04:40 +0800

    new folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head
+    <head>
         <title>Laravel</title>
-        <link rel="stylesheet" href="landpage.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="css/landpage.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>     
     </head>
     <body class="antialiased">
     <div class="topnav">
