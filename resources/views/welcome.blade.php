8048b4b4f54e822cfe2c900f64734da119a76682
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 11:37:14 +0800

    update header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,10 @@
     <head>
         <title>Home</title>
         <link rel="stylesheet" href="css/landpage.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>     
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.css"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css"/>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.js"></script>
-
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css" rel="stylesheet">
     </head>
     
     <body class="antialiased">
@@ -93,47 +90,59 @@
       </nav>
     
     <header>
-        <div class="owl-carousel owl-theme">
-            <div class="item">
-                <img src="https://cdn.pixabay.com/photo/2015/05/31/10/51/technology-791029_960_720.jpg" alt="">
-                <div class="cover">
-                    <div class="container">
-                        <div class="header-content">
-                            <div class="line"></div>
-                            <h2>Reinvent Business Experience with</h2>
-                            <h1>Our Global and Integrated Solutions</h1>
-                            <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into business</h4>
+        <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="mask flex-center">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-md-7 col-12 order-md-1 order-2">
+                                    <h4>Present your <br>
+                                    awesome product</h4>
+                                    <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                                    necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+                                    <a href="#">BUY NOW</a> 
+                                </div>
+                                <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/NKvkfTT.png" class="mx-auto" alt="slide"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>                    
-            <div class="item">
-                <img src="https://cdn.pixabay.com/photo/2017/10/17/19/11/fantasy-2861815_960_720.jpg" alt="">
-                <div class="cover">
-                    <div class="container">
-                        <div class="header-content">
-                            <div class="line animated bounceInLeft"></div>
-                            <h2>Reinvent Digital Experience with</h2>
-                            <h1>Our Digital and Intelligent Solutions</h1>
-                            <h4>We help entrepreneurs, start-ups and enterprises shape their dreams into profits</h4>
+                <div class="carousel-item">
+                    <div class="mask flex-center">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-md-7 col-12 order-md-1 order-2">
+                                    <h4>Present your <br>
+                                    awesome product</h4>
+                                    <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                                    necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+                                    <a href="#">BUY NOW</a> 
+                                </div>
+                                <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/duWgXRs.png" class="mx-auto" alt="slide"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>                
-            <div class="item">
-                <img src="https://cdn.pixabay.com/photo/2017/07/17/23/51/wormhole-2514312_960_720.jpg" alt="">
-                <div class="cover">
-                    <div class="container">
-                        <div class="header-content">
-                            <div class="line animated bounceInLeft"></div>
-                            <h2>Reinvent Client Experience with</h2>
-                            <h1>Our CRM and Software Solutions</h1>
-                            <h4>We help entrepreneurs, start-ups and enterprises shape their leads into clients</h4>
+                <div class="carousel-item">
+                    <div class="mask flex-center">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-md-7 col-12 order-md-1 order-2">
+                                    <h4>Present your <br>
+                                    awesome product</h4>
+                                    <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                                    necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+                                    <a href="#">BUY NOW</a> 
+                                </div>
+                                <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/DGkR4OQ.png" class="mx-auto" alt="slide"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+          <!--slide end--> 
     </header>
 
     <!-- Page Content -->
