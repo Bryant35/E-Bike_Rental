a07dd05ab9b55c87f9ea5cd4c475068285bf1b4d
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 12:42:41 +0800

    move css link

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -37,8 +37,9 @@
     </section>
     
     <!--footer-->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    
     <footer class="section footer-classic context-dark bg-image" style="background: #2d3246;">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <div class="container">
