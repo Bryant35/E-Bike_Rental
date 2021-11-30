2a35fa62e666b2b9b5b0b1d154295f244f7dbf86
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 12:51:20 +0800

    hide link

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -39,9 +39,9 @@
     <!--footer-->
     
     <footer class="section footer-classic context-dark bg-image" style="background: #2d3246;">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
         <div class="container">
             <div class="row row-30">
                 <div class="col-md-4 col-xl-5">
