006c770fc19e7f760ce1406c2b3972ef76811244
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 11:28:43 +0800

    landingpage new

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Bike Rental Website</title>
+    <link rel="stylesheet" href="public/css/landingpage.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"><img src="img/ebike_logo.png" alt=""></a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#ourservice">Our Service</a></li>
+            <li><a href="#contact">contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+        <div class="header-btn">
+            <a href="#login" class="log-in">Log In</a>
+            <a href="#register" class="register">Register</a>
+        </div>
+    </header>
+    <script src="public/js/landpage.js"></script>
+</body>
+</html>
