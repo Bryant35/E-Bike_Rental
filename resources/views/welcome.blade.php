48c94ec94d2a69208aff8f6cf231e2381c167ccb
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 10:44:31 +0800

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <title>Home</title>
-        <link rel="stylesheet" href="public/css/landpage.css">
+        <link rel="stylesheet" href="css/landpage.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>     
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css"/>
