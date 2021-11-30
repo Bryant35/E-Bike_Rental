788184af7cdad1c453051b934fdcdefbeee9e4fb
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 13:07:44 +0800

    new tes

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -31,7 +31,7 @@
 
     <!--home-->
     <section class="home" id="home">
-        <img src="img/bgpage.png" alt="">
+        <img class="img" src="img/bgpage.png" alt="">
         <div class="text">
             <h1><span>Welcome</span><br>Quick Access E-bike Rental Platform</h1>
         </div>
