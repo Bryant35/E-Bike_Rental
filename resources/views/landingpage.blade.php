75aa8bc1cf29c6e02e334c783e218beafeb0a628
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 13:57:32 +0800

    tes

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -31,8 +31,13 @@
 
     <!--home-->
 <<<<<<< HEAD
+<<<<<<< HEAD
     <section class="home">
         <!--<img class="img" src="img/bgpage.png" alt="">-->
+=======
+    <section class="home" id="home">
+        <img class="img" src="img/bgpage.png" alt="">
+>>>>>>> parent of 6e7e1bf (new landing)
 =======
     <section class="home" id="home">
         <img class="img" src="img/bgpage.png" alt="">
