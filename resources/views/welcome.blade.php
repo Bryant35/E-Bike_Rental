95b8fc80e480bb5014f8f777e9a1d815d27ac2d0
Bryant35 <banthony@student.ciputra.ac.id>
2021-11-30 10:25:56 +0800

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>Laravel</title>
+        <title>Home</title>
         <link rel="stylesheet" href="css/landpage.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>     
     </head>
