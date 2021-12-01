dff715bbb646ff63145bb6cbd2693e27b23b652f
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-01 16:24:13 +0800

    update footer

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
     <!--For Footer-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
@@ -46,7 +48,7 @@
     
     <div><h1 id="howtop">HOW TO RENT OUR BIKE</h1></div>
  
-    <section id="howto">
+    <section id="howto" class ="col-lg-4 col-md-6 mb-4 mb-lg-0 col-xl-12">
             <div class="howtopics">
             <img class="ONE" src="img/one.png"  alt="">
             <p class="ONEp1">FIND THE PERFECT LOCATION AND BIKE(S)</p>
@@ -101,69 +103,59 @@
     
 
     <!--footer-->
-    <footer class="container-fluid bg-grey py-5">
-        <div class="container">
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="row">
-                    <div class="col-md-6 ">
-                        <div class="logo-part">
-                            <img src="https://i.ibb.co/sHZz13b/logo.png" class="w-50 logo-footer" >
-                            <p>7637 Laurel Dr. King Of Prussia, PA 19406</p>
-                            <p>Use this tool as test data for an automated system or find your next pen</p>
-                        </div>
+    <footer class="bg-white">
+        <div class="container py-5">
+            <div class="row py-4">
+                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="img/logo.png" alt="" width="180" class="mb-3">
+                <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                <ul class="list-inline mt-4">
+                    <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fa fa-pinterest"></i></a></li>
+                    <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fa fa-vimeo"></i></a></li>
+                </ul>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                <h6 class="text-uppercase font-weight-bold mb-4">Shop</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a href="#" class="text-muted">For Women</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">For Men</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">Stores</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">Our Blog</a></li>
+                </ul>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a href="#" class="text-muted">Login</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">Register</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">Wishlist</a></li>
+                    <li class="mb-2"><a href="#" class="text-muted">Our Products</a></li>
+                </ul>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-lg-0">
+                <h6 class="text-uppercase font-weight-bold mb-4">Newsletter</h6>
+                <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
+                <div class="p-1 rounded border">
+                    <div class="input-group">
+                    <input type="email" placeholder="Enter your email address" aria-describedby="button-addon1" class="form-control border-0 shadow-0">
+                    <div class="input-group-append">
+                        <button id="button-addon1" type="submit" class="btn btn-link"><i class="fa fa-paper-plane"></i></button>
                     </div>
-                    <div class="col-md-6 px-4">
-                        <h6> About Company</h6>
-                        <p>But horizontal lines can only be a full pixel high.</p>
-                        <a href="#" class="btn-footer"> More Info </a><br>
-                        <a href="#" class="btn-footer"> Contact Us</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-6 px-4">
-                       <h6> Help us</h6>
-                        <div class="row ">
-                            <div class="col-md-6">
-                                <ul>
-                                    <li> <a href="#"> Home</a> </li>
-                                    <li> <a href="#"> About</a> </li>
-                                    <li> <a href="#"> Service</a> </li>
-                                    <li> <a href="#"> Team</a> </li>
-                                    <li> <a href="#"> Help</a> </li>
-                                    <li> <a href="#"> Contact</a> </li>
-                                </ul>
-                            </div>
-                            <div class="col-md-6 px-4">
-                                <ul>
-                                    <li> <a href="#"> Cab Faciliy</a> </li>
-                                    <li> <a href="#"> Fax</a> </li>
-                                    <li> <a href="#"> Terms</a> </li>
-                                    <li> <a href="#"> Policy</a> </li>
-                                    <li> <a href="#"> Refunds</a> </li>
-                                    <li> <a href="#"> Paypal</a> </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 ">
-                        <h6> Newsletter</h6>
-                        <div class="social">
-                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                        </div>
-                        <form class="form-footer my-3">
-                            <input type="text"  placeholder="search here...." name="search">
-                            <input type="button" value="Go" >
-                        </form>
-                        <p>That's technology limitation of LCD monitors</p>
-                    </div>
                 </div>
             </div>
         </div>
-    </footer>
+    
+        <!-- Copyrights -->
+        <div class="bg-light py-4">
+            <div class="container text-center">
+                <p class="text-muted mb-0 py-2">© 2019 Bootstrapious All rights reserved.</p>
+            </div>
+        </div>
+      </footer>
         <!-- Footer -->
             
     </div>
