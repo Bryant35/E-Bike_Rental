65370ee019877c4f705a4855fe8eb83be7e88a69
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-01 16:39:21 +0800

    Update

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -33,33 +33,36 @@
 
     <!--home-->
  
-    
+    <div class="container"> 
+        <div class="row">
             <div class="banner">
                 <img src="img/bgpage.png" alt="img">
                 <h2 class="heading">Welcome<br>Quick Access E-bike Rental Platform</h2>
-                <a href="#"  class="heading" >RENT NOW</a>
+                <a href="#"  class="rent" >RENT NOW</a>
             </div>
+        </div>
+    </div>
     <!--how to rent bek-->
     
     <div><h1 id="howtop">HOW TO RENT OUR BIKE</h1></div>
  
-    <section id="howto" class = "container-fluid">
+    <section id="howto" >
             <div class="howtopics">
-            <img class="ONE" src="img/one.png"  alt="">
-            <p class="ONEp1">FIND THE PERFECT LOCATION AND BIKE(S)</p>
-            <div><p class="ONEpsmall">CHOOSE BIKE RENTAL LOCATIONS, PRICES, BIKE VARIANTS. EVERYTHING IN ONE PLACE!</p></div>
+                <img class="ONE" src="img/one.png"  alt="">
+                <p class="ONEp1">FIND THE PERFECT LOCATION AND BIKE(S)</p>
+                <div><p class="ONEpsmall">CHOOSE BIKE RENTAL LOCATIONS, PRICES, BIKE VARIANTS. EVERYTHING IN ONE PLACE!</p></div>
             </div>
 
             <div class="howtopics">
-            <img class="ONE" src="img/two.png"  alt="">
-            <p class="ONEp1">RENT YOUR BIKE!</p>
-            <div><p class="ONEpsmall">AFTER CHOOSING YOUR LOCATION AND BIKE, OUR WEBSITE WILL GUIDE YOU RENT OUR BIKES.</p></div>
+                <img class="ONE" src="img/two.png"  alt="">
+                <p class="ONEp1">RENT YOUR BIKE!</p>
+                <div><p class="ONEpsmall">AFTER CHOOSING YOUR LOCATION AND BIKE, OUR WEBSITE WILL GUIDE YOU RENT OUR BIKES.</p></div>
             </div>
 
             <div class="howtopics">
-            <img class="ONE" src="img/three.png"  alt="">
-            <p class="ONEp1">THE BIKE IS READY TO GO!</p>
-            <div><p class="ONEpsmall">YOU WILL BE ABLE TO ENJOY YOUR RIDE</p></div>
+                <img class="ONE" src="img/three.png"  alt="">
+                <p class="ONEp1">THE BIKE IS READY TO GO!</p>
+                <div><p class="ONEpsmall">YOU WILL BE ABLE TO ENJOY YOUR RIDE</p></div>
             </div>
 
         </section>
@@ -92,70 +95,26 @@
         <div class="box"><img src="img/TP.png" alt=""></div>
         <!--<div class="top-left5">Tunjungan plaza</div>-->
 
-
     </div>
     </section>
     
 
     <!--footer-->
-    <footer class="bg-white">
+    <footer class="footer">
         <div class="container py-5">
-            <div class="row py-4">
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="img/logo.png" alt="" width="180" class="mb-3">
-                <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-                <ul class="list-inline mt-4">
-                    <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fa fa-pinterest"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fa fa-vimeo"></i></a></li>
-                </ul>
-                </div>
-                <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                <h6 class="text-uppercase font-weight-bold mb-4">Shop</h6>
-                <ul class="list-unstyled mb-0">
-                    <li class="mb-2"><a href="#" class="text-muted">For Women</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">For Men</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">Stores</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">Our Blog</a></li>
-                </ul>
-                </div>
-                <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
-                <ul class="list-unstyled mb-0">
-                    <li class="mb-2"><a href="#" class="text-muted">Login</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">Register</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">Wishlist</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted">Our Products</a></li>
-                </ul>
-                </div>
-                <div class="col-lg-4 col-md-6 mb-lg-0">
-                <h6 class="text-uppercase font-weight-bold mb-4">Newsletter</h6>
-                <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
-                <div class="p-1 rounded border">
-                    <div class="input-group">
-                    <input type="email" placeholder="Enter your email address" aria-describedby="button-addon1" class="form-control border-0 shadow-0">
-                    <div class="input-group-append">
-                        <button id="button-addon1" type="submit" class="btn btn-link"><i class="fa fa-paper-plane"></i></button>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
+            
         </div>
     
         <!-- Copyrights -->
         <div class="bg-light py-4">
             <div class="container text-center">
-                <p class="text-muted mb-0 py-2">© 2019 Bootstrapious All rights reserved.</p>
+                <p class="text-muted mb-0 py-2">© 2021 E-Bike Rental All rights reserved.</p>
             </div>
         </div>
-      </footer>
-        <!-- Footer -->
-            
-    </div>
+    </footer>
+    <!-- Footer -->
     
-
+    </div>
 </body>
 
 </html>
