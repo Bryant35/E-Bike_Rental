b31ac130740203c84809d080d23c2019655c33c3
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-01 15:51:20 +0800

    php commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -47,19 +47,19 @@
  
     <section id="howto">
             <div class="howtopics">
-            <img class="ONE" src="img/one.png" style=" width: '100px'; height: '100px'" alt="">
+            <img class="ONE" src="img/one.png"  alt="">
             <p class="ONEp1">FIND THE PERFECT LOCATION AND BIKE(S) FOR YOU</p>
             <p class="ONEpsmall">CHOOSE BIKE RENTAL LOCATIONS, PRICES, BIKE VARIANTS. EVERYTHING IN ONE PLACE!</p>
             </div>
 
             <div class="howtopics">
-            <img class="ONE" src="img/two.png" style=" width: '100px'; height: '100px'" alt="">
+            <img class="ONE" src="img/two.png"  alt="">
             <p class="ONEp1">RENT YOUR BIKE!</p>
             <p class="ONEpsmall">AFTER CHOOSING YOUR LOCATION AND BIKE, OUR WEBSITE WILL GUIDE YOU RENT OUR BIKES.</p>
             </div>
 
             <div class="howtopics">
-            <img class="ONE" src="img/three.png" style=" width: '100px'; height: '100px'" alt="">
+            <img class="ONE" src="img/three.png"  alt="">
             <p class="ONEp1">THE BIKE IS READY TO GO!</p>
             <p class="ONEpsmall">YOU WILL BE ABLE TO ENJOY YOUR RIDE</p>
             </div>
@@ -237,6 +237,60 @@
           <!-- Copyright -->
         </footer>
         <!-- Footer -->
+          <!-- Site footer -->
+          <!-- Site footer -->
+            <footer class="site-footer">
+                <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                    <h6>About</h6>
+                    <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+                    </div>
+        
+                    <div class="col-xs-6 col-md-3">
+                    <h6>Categories</h6>
+                    <ul class="footer-links">
+                        <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+                        <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+                        <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+                        <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+                        <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+                        <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                    </ul>
+                    </div>
+        
+                    <div class="col-xs-6 col-md-3">
+                    <h6>Quick Links</h6>
+                    <ul class="footer-links">
+                        <li><a href="http://scanfcode.com/about/">About Us</a></li>
+                        <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+                        <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+                        <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                    </ul>
+                    </div>
+                </div>
+                <hr>
+                </div>
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+                <a href="#">Scanfcode</a>.
+                    </p>
+                    </div>
+        
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+                    </ul>
+                    </div>
+                </div>
+                </div>
+        </footer>
     </div>
     
 
