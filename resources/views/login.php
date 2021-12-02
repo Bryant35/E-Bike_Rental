782edd2ce64faf96096617e57f74f5e27cbbc79f
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-02 19:26:20 +0800

    Update l;ogin page

--- resources/views/login.php
+++ resources/views/login.php
@@ -14,26 +14,6 @@
     <a href="https://front.codes/" class="logo" target="_blank"> <img src="https://assets.codepen.io/1462889/fcy.png" alt=""> </a>
     <div class="section">
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card">
-                        <form onsubmit="event.preventDefault()" class="box">
-                            <h1>Login</h1>
-                            <p class="text-muted"> Please enter your login and password!</p> <input type="text" name="" placeholder="Username"> <input type="password" name="" placeholder="Password"> <a class="forgot text-muted" href="#">Forgot password?</a> <input type="submit" name="" value="Login" href="#">
-                            <div class="col-md-12">
-                                <ul class="social-network social-circle">
-                                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
-                                </ul>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div><a href="https://front.codes/" class="logo" target="_blank"> <img src="https://assets.codepen.io/1462889/fcy.png" alt=""> </a>
-    <div class="section">
-        <div class="container">
             <div class="row full-height justify-content-center">
                 <div class="col-12 text-center align-self-center py-5">
                     <div class="section pb-5 pt-5 pt-sm-2 text-center">
