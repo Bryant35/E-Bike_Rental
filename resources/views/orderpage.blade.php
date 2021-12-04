97d6ee58d4378daf71ead193748b5c0a264180e6
HiItsNiko <74892226+HiItsNiko@users.noreply.github.com>
2021-12-04 15:10:51 +0700

    small update

---
+++ resources/views/orderpage.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--header-->
+    <nav>
+        <a href="#" class="logo"><img src="img/ebike_logo.png" alt=""></a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#ourservice">Our Service</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+        <div class="nav-btn">
+            <a href="login.php" class="log-in">Log In</a>
+            <a href="#register" class="register">Register</a>
+        </div>
+    </nav>
+
+
+
+</body>
+</html>
