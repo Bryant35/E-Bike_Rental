c491cc7b1e3519c789749cef513768c1738bed14
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-06 12:28:30 +0800

    Update Title

--- resources/views/signuppage.blade.php
+++ resources/views/signuppage.blade.php
@@ -3,7 +3,7 @@
 <html lang="en" dir="ltr">
       <head>
          <meta charset="utf-8">
-         <title>Multi Step Form | CodingNepal</title>
+         <title>Register</title>
          <link rel="stylesheet" href="css/signuppage.css">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
       </head>
