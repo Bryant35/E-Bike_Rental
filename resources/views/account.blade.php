c1bf1b0494414c0574c67be2dd87da8dbc71df8c
Bryant35 <banthony@student.ciputra.ac.id>
2021-12-10 12:23:55 +0800

    update account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link rel="stylesheet" href="css/account.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
